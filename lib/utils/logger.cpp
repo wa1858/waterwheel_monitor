1dd646398b1162f2fec4c409bc033d2da8a51f15
sa-fx <stewart.alexander2018@gmail.com>
2023-12-29 22:46:08 +0000

    Refactor, and included milliseconds in timestamp

--- lib/utils/logger.cpp
+++ lib/utils/logger.cpp
@@ -10,71 +10,67 @@ Logger::~Logger() {
   fclose(this->log_file_);
 }
 
-// TODO - Refactor required
 void Logger::log(LogLevel logLevel, const char *message, ...) {
   if (this->level_ <= logLevel) {
-    std::stringstream ss_terminal;
-    std::stringstream ss_log_file;
-    std::string time = getTimeStamp();
-    ss_terminal << "[" << time << "] ";
-    ss_log_file << "[" << time << "] ";
+    std::string time = "[" + getTimeStamp() + "] ";
+    std::string head, head_coloured;
 
     FILE *file = stdout;
     switch (logLevel) {
       case LogLevel::kDebug:
-        ss_terminal << kColourFormatDebug << "[DEBUG]   " << kColourFormatReset
-                    << " ";
-        ss_log_file << "[DEBUG]    ";
+        head = "[DEBUG]    ";
+        head_coloured = kColourFormatDebug + head + kColourFormatReset;
         break;
       case LogLevel::kInfo:
-        ss_terminal << kColourFormatInfo << "[INFO]    " << kColourFormatReset
-                    << " ";
-        ss_log_file << "[INFO]     ";
+        head = "[INFO]     ";
+        head_coloured = kColourFormatInfo + head + kColourFormatReset;
         break;
       case LogLevel::kWarning:
-        ss_terminal << kColourFormatWarning << "[WARNING] "
-                    << kColourFormatReset << " ";
-        ss_log_file << "[WARNING]  ";
+        head = "[WARNING]  ";
+        head_coloured = kColourFormatWarning + head + kColourFormatReset;
         break;
       case LogLevel::kFatal:
-        ss_terminal << kColourFormatFatal << "[ERROR]   " << kColourFormatReset
-                    << " ";
-        ss_log_file << "[ERROR]    ";
+        head = "[ERROR]    ";
+        head_coloured = kColourFormatFatal + head + kColourFormatReset;
         break;
       case LogLevel::kNone:
       default:
         break;
     }
-
-    ss_terminal << message;
-    ss_log_file << message;
-    std::string output_terminal = ss_terminal.str();
-    std::string output_log_file = ss_log_file.str();
+    std::string output = time + head_coloured + message;
 
     va_list args;
     va_start(args, message);
-    vfprintf(file, output_terminal.c_str(), args);
+    vfprintf(file, output.c_str(), args);
     fprintf(file, "\n");
     va_end(args);
 
-    // TODO - Why do some values print as -0.0, instead of 0.0
     if (log_file_ != nullptr) {
+      output = time + head + message;
       va_list args;
       va_start(args, message);
-      vfprintf(log_file_, output_log_file.c_str(), args);
+      vfprintf(log_file_, output.c_str(), args);
       fprintf(log_file_, "\n");
       va_end(args);
     }
   }
 }
 
-// TODO - Print to the millisecond
 std::string Logger::getTimeStamp() {
-  auto now = std::chrono::system_clock::now();
-  auto in_time_t = std::chrono::system_clock::to_time_t(now);
+  auto currentTime = std::chrono::system_clock::now();
+  auto currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
+  auto *timeInfo = std::localtime(&currentTime_t);
 
-  std::stringstream ss;
-  ss << std::put_time(std::localtime(&in_time_t), "%Y/%m/%d %X");
+  // Total number of milliseconds in time % 1000 = milliseconds of timestamp
+  auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
+                          currentTime.time_since_epoch())
+                          .count() %
+                      1000;
+
+  // Format the time as a string
+  std::ostringstream ss;
+  ss << std::put_time(timeInfo, "%Y/%m/%d %H:%M:%S");
+  ss << "." << std::setfill('0') << std::setw(3) << milliseconds;
   return ss.str();
 }
 
@@ -95,9 +91,8 @@ void Logger::createLogFile() {
 
   std::string log_file_path =
       kLogFileDirectory + "/" + kLogFilePrefix + date + kLogFileType;
-  // TODO - Open a file without overwriting it if it already exists
-  // log_file_.open(log_file_path);
-  log_file_ = fopen(log_file_path.c_str(), "w");
+  // Open the file, appending if it already exists
+  log_file_ = fopen(log_file_path.c_str(), "a");
   if (log_file_ == nullptr) {
     this->log(LogLevel::kWarning, "Unable to create log file on disk");
     return;
